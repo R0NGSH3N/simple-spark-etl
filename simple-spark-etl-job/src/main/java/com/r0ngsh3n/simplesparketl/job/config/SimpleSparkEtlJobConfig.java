c87de03e9a66871a4167881570b76aaed8cd104f
r0ngsh3n <rong.shen@outlook.com>
2021-06-18 15:56:17 -0400

    remove the dependency on typesafe config, switch to use gson for config

--- simple-spark-etl-job/src/main/java/com/r0ngsh3n/simplesparketl/job/config/SimpleSparkEtlJobConfig.java
+++ simple-spark-etl-job/src/main/java/com/r0ngsh3n/simplesparketl/job/config/SimpleSparkEtlJobConfig.java
@@ -3,6 +3,9 @@ package com.r0ngsh3n.simplesparketl.job.config;
 //import com.fasterxml.jackson.core.JsonProcessingException;
 //import com.fasterxml.jackson.databind.ObjectMapper;
 //import com.r0ngsh3n.simplesparketl.job.config.SparkConfig;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigBeanFactory;
 import com.typesafe.config.ConfigFactory;
@@ -10,8 +13,13 @@ import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 import com.typesafe.config.Optional;
 
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 @Data
 @Builder
@@ -88,15 +96,18 @@ public class SimpleSparkEtlJobConfig {
         private String destinationDB;
     }
 
+    @SneakyThrows
     public static SimpleSparkEtlJobConfig loadConfig(){
-        Config config = ConfigFactory.load();
-        SimpleSparkEtlJobConfig simpleSparkEtlJobConfig = ConfigBeanFactory.create(config, SimpleSparkEtlJobConfig.class);
+        Gson gson = new Gson();
+        Reader reader = new InputStreamReader(Objects.requireNonNull(SimpleSparkEtlJobConfig.class.getResourceAsStream("/application.conf"), "Reading application.conf is Null."));
+//        Reader reader = Files.newBufferedReader(Paths.get(configFilePath));
+        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
 
-//        try {
-//            log.info("{}", new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(simpleSparkEtlFilWatcherConfig));
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
+        SimpleSparkEtlJobConfig simpleSparkEtlJobConfig = SimpleSparkEtlJobConfig.builder().build();
+
+        simpleSparkEtlJobConfig.sources = gson.fromJson(jsonObject.get("sourceConfig"), new TypeToken<List<SourceConfig>>(){}.getType());
+        simpleSparkEtlJobConfig.sparkConfig = gson.fromJson(jsonObject.get("sparkConfig"), SparkConfig.class);
+        simpleSparkEtlJobConfig.cacheConfig = gson.fromJson(jsonObject.get("cacheConfig"),CacheConfig.class);
 
         return simpleSparkEtlJobConfig;
     }
